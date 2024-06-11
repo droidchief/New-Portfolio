c47a04d8c6ee7c19933dffe4ee1275867bec60a8
Vicviral <lovedayv7@gmail.com>
2024-06-11 15:49:42 +0100

    update: contact mail

--- components/Utils/ContactIconsMobile.tsx
+++ components/Utils/ContactIconsMobile.tsx
@@ -43,10 +43,10 @@ const ContactIconsMobile = () => {
         <div className="list-none">
           <div className="md:rotate-90">
             <Link
-              href={`mailto:xeuxdev@gmail.com`}
+              href={`mailto:lovedayv7@gmail.com`}
               className="text-sm font-semibold duration-200 text-text hover:text-cyan font-pop "
             >
-              xeuxdev@gmail.com
+              lovedayv7@gmail.com
             </Link>
           </div>
         </div>
