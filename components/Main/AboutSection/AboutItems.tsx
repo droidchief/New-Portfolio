3e68d7926e7ae56b30f4b9ed791b9db6e1c92b3b
Vicviral <lovedayv7@gmail.com>
2024-06-11 07:24:37 +0100

    Initial commit

---
+++ components/Main/AboutSection/AboutItems.tsx
@@ -0,0 +1,93 @@
+import { m } from "framer-motion";
+import Image from "next/image";
+import { aboutVariant } from "../../../variants/aboutVariant";
+
+const AboutItems = () => {
+  return (
+    <>
+      {" "}
+      <div className="flex flex-col items-center justify-center lg:flex-row lg:space-x-10 ">
+        {/* text */}
+        <div className="text lg:flex-[45%]">
+          <m.h1
+            className="flex items-start mb-6 space-x-2 text-xl font-normal text-white lg:text-2xl font-pop lg:space-x-3"
+            initial={{ opacity: 0, y: 20 }}
+            variants={aboutVariant}
+            whileInView={aboutVariant.heading}
+            viewport={{ once: true }}
+          >
+            <p>Hi{"👋, "} I&apos;m</p>
+            <p className="relative font-semibold font-pop text-cyan w-fit">
+              Victor Loveday,
+            </p>
+          </m.h1>
+          {/* info */}
+          <div className="mb-10 md:mb-12 lg:mb-0">
+            <m.p
+              className="mb-6 text-lg font-normal text-text lg:text-xl"
+              variants={aboutVariant}
+              initial={{ opacity: 0, y: 25 }}
+              whileInView={aboutVariant.p1}
+              viewport={{ once: true }}
+            >
+              I'm a Mobile Developer with over 5 years of professional
+              experience and a great stack of success. I have been building
+              software solutions for EduTech, Health Care, Fintech, and
+              Businesses for the past five (5) years, and also possess vast
+              knowledge of software development and project management.
+            </m.p>
+            <m.p
+              className="mb-6 text-lg font-normal text-text lg:text-xl"
+              variants={aboutVariant}
+              initial={{ opacity: 0, y: 25 }}
+              whileInView={aboutVariant.p2}
+              viewport={{ once: true }}
+            >
+              I'm adept at bringing all team members together to reach a common
+              goal on time and under budget, conceptualizing software solutions
+              with the best technology, design theory, and a large dose of
+              creativity.
+            </m.p>
+            <m.p
+              className="mb-6 text-lg font-normal text-text lg:text-xl"
+              variants={aboutVariant}
+              initial={{ opacity: 0, y: 25 }}
+              whileInView={aboutVariant.p2}
+              viewport={{ once: true }}
+            >
+              I have helped many new programmers (virtually and physically)
+              become proficient in their chosen programming language. I'm
+              committed to viable and easily functional software solutions for
+              clients.
+            </m.p>
+          </div>
+        </div>
+        {/* images/illustrsions */}
+        <m.div
+          className="lg:flex-[40%] w-full md:max-w-md relative cursor-pointer group"
+          initial={{ opacity: 0, y: 20 }}
+          variants={aboutVariant}
+          whileInView={aboutVariant.image}
+          viewport={{ once: true }}
+        >
+          <div className="w-full md:w-full h-[28rem] md:h-[37rem] lg:h-[30rem] xl:h-[34rem] relative rounded-md overflow-hidden border-2 border-cyan backdrop-blur-sm bg-cyan/20 z-20 ">
+            <div className="relative w-full h-full duration-500 grayscale group-hover:grayscale-0">
+              <Image
+                src={"/images/me.jpeg"}
+                alt="moi"
+                priority={true}
+                fill
+                quality={100}
+                placeholder={"blur"}
+                blurDataURL={"/images/me.jpeg"}
+              />
+            </div>
+          </div>
+          <div className="absolute h-[28rem] md:w-[105%] md:h-[37.5rem] lg:h-[30rem] xl:h-[34.5rem] w-full top-5 left-3 border-2 border-body z-10 bg-buttonBg rounded-lg group-hover:top-4 group-hover:left-3 duration-500"></div>
+        </m.div>
+      </div>
+    </>
+  );
+};
+
+export default AboutItems;
---
+++ components/Main/HeroSection/HeroSection.tsx
@@ -0,0 +1,102 @@
+import Text from "../../Text/Text";
+import { AboutIcon } from "../icons";
+import HeroNav from "./HeroNav";
+import AboutMeButton from "./AboutMeButton";
+import ResumeButton from "./ResumeButton";
+import { m } from "framer-motion";
+
+const HeroSection = () => {
+  return (
+    <div className="flex flex-col w-full">
+      {/* text */}
+      <div className="relative w-full mx-auto">
+        {/* name */}
+        <div className="w-full font-bold text-cyan font-pop">
+          <m.p
+            className="mb-4 text-lg font-pop md:text-xl md:mb-6"
+            initial={{ y: 20, opacity: 0 }}
+            animate={{
+              y: 0,
+              opacity: 1,
+              transition: { duration: 0.3, delay: 0.55 },
+            }}
+          >
+            Hi there, 👋I&apos;m
+          </m.p>
+          <m.h1
+            className="flex items-center space-x-5 pb-7 font-pop text-text text-3xl sm:text-4xl md:text-[5rem] md:leading-[1.3]"
+            initial={{ y: 20, opacity: 0 }}
+            animate={{
+              y: 0,
+              opacity: 1,
+              transition: { duration: 0.3, delay: 0.6 },
+            }}
+          >
+            <span className=" w-fit">Victor</span>
+            <span className="relative text-cyan before:absolute before:-bottom-2 before:left-0 before:w-full before:h-1 md:before:h-2 md:before:-bottom-3 before:block before:bg-cyan before:rounded-full">
+              Loveday.
+            </span>
+          </m.h1>
+        </div>
+        {/* brief about */}
+        <div className="w-full mb-6 ">
+          <m.p
+            className="text-text_Light text-3xl sm:text-4xl md:text-6xl font-semibold md:leading-[1.3]"
+            initial={{ y: 20, opacity: 0 }}
+            animate={{
+              y: 0,
+              opacity: 1,
+              transition: { duration: 0.3, delay: 0.7 },
+            }}
+          >
+            I build all things mobile.
+          </m.p>
+        </div>
+        {/* brief about info */}
+        <div className="max-w-[40.75rem]">
+          <m.p
+            className="text-text_Light text-base md:text-xl md:leading-[1.3] "
+            initial={{ y: 20, opacity: 0 }}
+            animate={{
+              y: 0,
+              opacity: 1,
+              transition: { duration: 0.3, delay: 0.8 },
+            }}
+          >
+            I'm a{" "}
+            <span className="text-cyan">
+              {" "}
+              Mobile Developer with over 5 years
+            </span>{" "}
+            of professional experience and a great stack of success. I have been
+            building software solutions for EduTech, Health Care, Fintech, and
+            Businesses for the past five (5) years, and also possess vast
+            knowledge of software development and project management. I'm adept
+            at bringing all team members together to reach a common goal on time
+            and under budget, conceptualizing software solutions with the best
+            technology, design theory, and a large dose of creativity. I have
+            helped many new programmers (virtually and physically) become
+            proficient in their chosen programming language. I'm committed to
+            viable and easily functional software solutions for clients.
+          </m.p>
+        </div>
+        {/* buttons */}
+        <m.div
+          className="flex items-center w-full mb-10 space-x-5 md:space-x-7 mt-11 md:mt-14"
+          initial={{ y: 20, opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            transition: { duration: 0.3, delay: 0.9 },
+          }}
+        >
+          <AboutMeButton />
+          <ResumeButton />
+        </m.div>
+        {/* btn2 */}
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
